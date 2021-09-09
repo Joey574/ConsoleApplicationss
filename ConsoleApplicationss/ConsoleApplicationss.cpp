294ed6aef7fa9d714d8dc03dd0b3af5b9b8b0d27
Joseph Soroka <sorokaj@rsd407.org>
2021-09-09 08:40:15 -0700

    Added introductionary comments

--- ConsoleApplicationss/ConsoleApplicationss.cpp
+++ ConsoleApplicationss/ConsoleApplicationss.cpp
@@ -1,7 +1,11 @@
 /*
 Author: Joey Soroka
-Title: Hello World
+Project Name: Hello World
+Filename: ConsoleApplicationss.cpp
 Purpose: Introductory code to print "hello world"
+Pseudocode: Prints 3 lines of words saying "Hello World", "Hello Eric", and "Hello Brooks"
+Maintenance Log: 
+9/9/21  set up basic code infrastructure and set up main purpose, printing 3 lines of words, plus a space between lines and file path
 
 */
 #include <stdio.h>
