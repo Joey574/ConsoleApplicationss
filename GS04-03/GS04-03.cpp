06d928f10a1ec50bf02c87d1ec13d0b42cea21e6
Joseph Soroka <sorokaj@rsd407.org>
2021-12-03 09:06:40 -0800

    12/3

--- GS04-03/GS04-03.cpp
+++ GS04-03/GS04-03.cpp
@@ -19,9 +19,36 @@ using namespace std;
 
 int main()
 {
-    string input;
+	string input;
+	int spaces = 0;
+	int newline = 0;
+	int numbers = 0;
+	int tabs = 0;
 
-    printf("Enter string to be counted\n");
-    cin >> input;
-    cout << input.size() << endl;
+	printf("Enter string to count, use tilde to represent end.\n");
+	getline(cin, input, '~');
+	cout << 
+
+	for (int i = 0; i < input.length(); i++)
+	{
+
+		if (input[i] == ' ')
+		{
+			spaces+= 1;
+		}
+		else if (input[i] == '\t')
+		{
+			tabs+= 1;
+		}
+		else if (input[i] == '\n')
+		{
+			newline+= 1;
+		}
+		else if (input[i] >= '0' && input[i] <= '9')
+		{
+			numbers+= 1;
+		}
+	}
+	printf("%i spaces, %i numbers, %i new lines, %i tabs.\n", spaces, numbers, newline, tabs);
+	return 0;
 }
