daa17bc2881cc3faf6328191856daf53cdd9bc2b
Joseph Soroka <sorokaj@rsd407.org>
2021-12-06 08:55:59 -0800

    12/6/21

--- GS04-03/GS04-03.cpp
+++ GS04-03/GS04-03.cpp
@@ -1,10 +1,12 @@
 /*
 Author: Joey Soroka
 Project Name: GS04-03
-Purpose: Create a program to count # of characters in a string
-Pseudocode: 
+Purpose: Create a program to count # of characters in a string as well as some other data
+Pseudocode: Asks user for input and to use a tilde tell the code where the input ends, it then counts the # of spaces, newlines, numbers, and tabs,
+			before outputting them as well as # of characters.
 Maintenance Log:
 12/2/21:    Started project
+12/6/21:	Fixed issues with code, now works fine
 */
 
 #include <stdio.h>
@@ -19,36 +21,37 @@ using namespace std;
 
 int main()
 {
-	string input;
+	string text;
 	int spaces = 0;
-	int newline = 0;
 	int numbers = 0;
 	int tabs = 0;
+	int newlines = 0;
 
 	printf("Enter string to count, use tilde to represent end.\n");
-	getline(cin, input, '~');
-	cout << 
+	getline(cin, text, '~');
+	system("CLS");
 
-	for (int i = 0; i < input.length(); i++)
+	for (int i = 0; i < text.length(); i++)
 	{
 
-		if (input[i] == ' ')
+		if (text[i] == ' ')
 		{
-			spaces+= 1;
+			spaces += 1;
 		}
-		else if (input[i] == '\t')
+		else if (text[i] == '\t')
 		{
-			tabs+= 1;
+			tabs += 1;
 		}
-		else if (input[i] == '\n')
+		else if (text[i] == '\n')
 		{
-			newline+= 1;
+			newlines += 1;
 		}
-		else if (input[i] >= '0' && input[i] <= '9')
+		else if (text[i] >= '0' && text[i] <= '9')
 		{
-			numbers+= 1;
+			numbers += 1;
 		}
+
 	}
-	printf("%i spaces, %i numbers, %i new lines, %i tabs.\n", spaces, numbers, newline, tabs);
+	cout << "Characters: " << text.length() << "\nSpaces: " << spaces << "\nLines: " << newlines << "\nNumbers: " << numbers << "\nTabs: " << tabs << endl;
 	return 0;
 }
