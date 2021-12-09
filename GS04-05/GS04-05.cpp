5f34c4df49b826a374db159cc5841a48673dd8d8
Joseph Soroka <sorokaj@rsd407.org>
2021-12-09 08:58:27 -0800

    12/9/21

---
+++ GS04-05/GS04-05.cpp
@@ -0,0 +1,74 @@
+/*
+Author: Joey Soroka
+File Name: GS04-05.cpp
+Project Name: GS04-05 (looping maze)
+Purpose: Similar to the maze project the looping maze will funciton the same however won't close the doors behind you
+Pseudocode:
+Maintenance Log:
+12/9/21:    Started the project
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string> // Needed for string
+#include <stdlib.h> // Needed for srand
+#include <time.h> // Needed for srand
+#include <conio.h>
+#include <vector>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	bool room1 = true;
+	bool room2 = false;
+	bool room3 = false;
+	bool room4 = false;
+	bool room5 = false;
+	bool room6 = false;
+	bool room7 = false;
+	bool room8 = false;
+	bool room9 = false;
+
+	printf("You are in a maze with 9 different rooms, try to find a way out!");
+	_getch();
+	system("CLS");
+
+	while (room9 == false)
+	{
+		if (room1 == true)
+		{
+
+		}
+		else if (room2 == true)
+		{
+
+		}
+		else if (room3 == true)
+		{
+
+		}
+		else if (room4 == true)
+		{
+
+		}
+		else if (room5 == true)
+		{
+
+		}
+		else if (room6 == true)
+		{
+
+		}
+		else if (room7 == true)
+		{
+
+		}
+		else if (room8 == true)
+		{
+
+		}
+	}
+
+}
+
