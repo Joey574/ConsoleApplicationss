23c7b7fe91de6764096cd6209168a31ac2db51ab
Joey574 <90403963+Joey574@users.noreply.github.com>
2021-12-13 14:11:51 -0800

    12/13/21

--- GS06-03/GS06-03.cpp
+++ GS06-03/GS06-03.cpp
@@ -17,7 +17,63 @@ Maintenance Log:
 #include <windows.h>
 using namespace std;
 
+string fortune(num);
+
 int main()
 {
+    int num;
+
+    printf("Enter a number from 1-10");
+    cin >> num;
     
+    string fort = fortune(num);
+    cout << fort;
+}
+
+string fortune(num)
+{
+    string fort;
+
+    if (num = 1)
+    {
+        fort = "You will be rich one day";
+    }
+    else if (num = 2)
+    {
+        fort = "You will become an astrnaut!";
+    }
+    else if (num = 3)
+    {
+        fort + "You will die a slow and painful death!";
+    }
+    else if (num = 4)
+    {
+
+    }
+    else if (num = 5)
+    {
+
+    }
+    else if (num = 6)
+    {
+
+    }
+    else if (num = 7)
+    {
+
+    }
+    else if (num = 8)
+    {
+
+    }
+    else if (num = 9)
+    {
+
+    }
+    else if (num = 10)
+    {
+
+    }
+
+    return 
 }
