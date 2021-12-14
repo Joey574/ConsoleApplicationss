2018365d47afaa0a4ac6d9e1f0d99e2f73a61ebc
Joseph Soroka <sorokaj@rsd407.org>
2021-12-14 10:51:02 -0800

    12/14/21

---
+++ GS06-04/GS06-04.cpp
@@ -0,0 +1,39 @@
+/*
+Author: Joey Soroka
+File Name: GS06-04.cpp
+Project Name: Problem GS06-04 (function maze)
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <windows.h>
+using namespace std;
+
+struct rooms();
+
+
+
+int main()
+{
+    
+}
+
+struct rooms()
+{
+    room1 = true;
+    room2 = false
+    room3 = false
+    room4 = false
+    room5 = false
+    room6 = false
+    room7 = false
+    room8 = false
+    room9 = false
+    room10 = false;
+    room11 = false
+    room12 = false;
+}
