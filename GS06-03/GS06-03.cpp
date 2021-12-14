35c5aa2dcd4c6c2331ce36922861eac5b740e68d
Joey574 <90403963+Joey574@users.noreply.github.com>
2021-12-14 08:32:21 -0800

    12/14/21

--- GS06-03/GS06-03.cpp
+++ GS06-03/GS06-03.cpp
@@ -22,12 +22,15 @@ string fortune(num);
 int main()
 {
     int num;
-
+    while(1)
+    {
+        
     printf("Enter a number from 1-10");
     cin >> num;
     
     string fort = fortune(num);
     cout << fort;
+    }
 }
 
 string fortune(num)
@@ -44,36 +47,36 @@ string fortune(num)
     }
     else if (num = 3)
     {
-        fort + "You will die a slow and painful death!";
+        fort = "You will die a slow and painful death!";
     }
     else if (num = 4)
     {
-
+        fort = "Uh oh... this isn't good, let's just move on shall we?";
     }
     else if (num = 5)
     {
-
+        fort = "Elon musk is gonna pass his company on to you";
     }
     else if (num = 6)
     {
-
+        fort = "You're gonna trip and fall in a volcano";
     }
     else if (num = 7)
     {
-
+        fort = "You're gonna live through a real life sharknado!";
     }
     else if (num = 8)
     {
-
+        fort = "You're gonna be responsible for the end of the world!";
     }
     else if (num = 9)
     {
-
+        fort = "You will be adbucted by aliens!";
     }
     else if (num = 10)
     {
-
+        fort = "You will abduct aliens!";
     }
 
-    return 
+    return fort;
 }
