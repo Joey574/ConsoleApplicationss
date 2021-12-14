52e930aecf3564e6f49666b54098ed896ab1aa57
Joseph Soroka <sorokaj@rsd407.org>
2021-12-14 10:44:12 -0800

    12/14/21

--- GS06-03/GS06-03.cpp
+++ GS06-03/GS06-03.cpp
@@ -19,7 +19,7 @@ Maintenance Log:
 #include <windows.h>
 using namespace std;
 
-string fortune(num);
+string fortune(int num);
 
 int main()
 {
@@ -27,17 +27,17 @@ int main()
     
     while(1)
     {
-        
-        printf("Enter a number from 1-10");
+        system("CLS");
+        printf("Enter a number from 1-10\n");
         cin >> num;
         
         string fort = fortune(num);
         cout << fort;
         
-        printf("Press 1 to exit, 2 to loop");
+        printf("Press 1 to exit, 2 to loop\n");
         cin >> num; // reuse num variable, code won't be affected
         
-        if (num = 1)
+        if (num == 1)
         {
          break;   
         }
@@ -46,49 +46,49 @@ int main()
     return 0;
 }
 
-string fortune(num)
+string fortune(int num)
 {
     string fort;
 
-    if (num = 1)
+    if (num == 1)
     {
-        fort = "You will be rich one day";
+        fort = "You will be rich one day\n";
     }
-    else if (num = 2)
+    else if (num == 2)
     {
-        fort = "You will become an astrnaut!";
+        fort = "You will become an astrnaut!\n";
     }
-    else if (num = 3)
+    else if (num == 3)
     {
-        fort = "You will die a slow and painful death!";
+        fort = "You will die a slow and painful death!\n";
     }
-    else if (num = 4)
+    else if (num == 4)
     {
-        fort = "Uh oh... this isn't good, let's just move on shall we?";
+        fort = "Uh oh... this isn't good, let's just move on shall we?\n";
     }
-    else if (num = 5)
+    else if (num == 5)
     {
-        fort = "Elon musk is gonna pass his company on to you";
+        fort = "Elon musk is gonna pass his company on to you\n";
     }
-    else if (num = 6)
+    else if (num == 6)
     {
-        fort = "You're gonna trip and fall in a volcano";
+        fort = "You're gonna trip and fall in a volcano\n";
     }
-    else if (num = 7)
+    else if (num == 7)
     {
-        fort = "You're gonna live through a real life sharknado!";
+        fort = "You're gonna live through a real life sharknado!\n";
     }
-    else if (num = 8)
+    else if (num == 8)
     {
-        fort = "You're gonna be responsible for the end of the world!";
+        fort = "You're gonna be responsible for the end of the world!\n";
     }
-    else if (num = 9)
+    else if (num == 9)
     {
-        fort = "You will be adbucted by aliens!";
+        fort = "You will be adbucted by aliens!\n";
     }
-    else if (num = 10)
+    else if (num == 10)
     {
-        fort = "You will abduct aliens!";
+        fort = "You will abduct aliens!\n";
     }
 
     return fort;
