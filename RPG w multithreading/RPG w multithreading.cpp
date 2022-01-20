1cf549c5f246d87a5330c4557085ec1149fe17cd
joey soroka <joeysoroka@outlook.com>
2022-01-20 14:48:59 -0800

    1/20/22

--- RPG w multithreading/RPG w multithreading.cpp
+++ RPG w multithreading/RPG w multithreading.cpp
@@ -39,38 +39,6 @@ int main()
 	player p;
 	string input;
 
-	ifstream save;
-	save.open("save.txt");
-	if (!save.is_open())
-	{
-		p.name = firstTimeIntroduction();
-	}
-	else
-	{
-		while (1)
-		{
-			system("CLS");
-			printf("It seems like you have a save, would you like to load it?\n1. Yes\n2. No\n");
-			cin >> input;
-			system("CLS");
-			if (input == "1")
-			{
-
-			}
-			else if (input == "2")
-			{
-				p.name = firstTimeIntroduction();
-			}
-			else
-			{
-				printf("Incorrect input please enter valid input\n");
-				Sleep(1000);
-				continue;
-			}
-		}
-		
-	}
-
 	printf("Menu:\n1: Start Game\n2: High Scores\n3: Help\n4: Credits\n5: Save/Load\n6: Exit\nYou Pick: ");
 	cin >> input;
 	if (input == "1")
@@ -101,6 +69,8 @@ int main()
 	{
 
 	}
+
+
 	
 }
 
