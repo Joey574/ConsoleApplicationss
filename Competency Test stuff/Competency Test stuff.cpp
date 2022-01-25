ac1d066ce40a92b12cb8cba0ef9d8617ce81d554
joey soroka <joeysoroka@outlook.com>
2022-01-24 17:57:15 -0800

    1/24/22

--- Competency Test stuff/Competency Test stuff.cpp
+++ Competency Test stuff/Competency Test stuff.cpp
@@ -16,43 +16,149 @@
 #include <fstream> // needed for files
 #include <cwchar>
 #include <algorithm> // needed for vector sort
-/*
-enum size {each = 1, dozen = 12, gross = 144 };
 
+using namespace std;
+
+// CT LOOPING
+// 1.)
+/*
 int main()
 {
-    size boxes;
-    size envelopes;
+	for (int i = 15, counter = 0; i < 126; i++)
+	{
+		
+		if (i != 25 && i != 30 && i != 35)
+		{
+			if (counter == 5)
+			{
+				printf("\n");
+				counter = 0;
+			}
+			cout << i << "\t";
+			counter++;
+		}
+	}
+}
+*/
+// 2.)
+/*
+int main()
+{
+	int iNum;
 
-    envelopes = dozen;
-    boxes = each;
+	printf("Enter a positive number: ");
+	cin >> iNum;
+
+	while (iNum < 0)
+	{
+		system("CLS");
+		printf("please enter a POSITIVE number: ");
+		cin >> iNum;
+	}
+}
+*/
+// 3.)
+/*
+int main()
+{
+	int iIncrement;
+	int iStart;
+	int iEnd;
+
+	printf("Please enter a starting value: ");
+	cin >> iStart;
+	printf("Please enter an ending value: ");
+	cin >> iEnd;
+	printf("Please enter the increment to change by: ");
+	cin >> iIncrement;
+	system("CLS");
+
+	do
+	{
+		iStart += iIncrement;
+	} while (iStart <= iEnd);
 }
 */
 
-struct person
-{
-    string firstName;
-    string lastName;
-
-    char middleInitial;
-
-    int age;
-
-    float height;
-};
-
+// CT STRINGS
+// 1.)
+/*
 int main()
 {
-    person p;
-
-    printf("Please enter your first name: ");
-    cin >> p.firstName;
-    printf("Please enter your middle initial: ");
-    cin >> p.middleInitial;
-    printf("Please enter your last name: ");
-    cin >> p.lastName;
-    printf("Please enter your age: ");
-    cin >> p.age;
-    printf("Please enter your height (use a . to seperate feet and inches, ex. 5.11 for 5 feet 11 inches): ");
-    cin >> p.height;
+	string sfName = "Joey";
+	cout << sfName[2];
+	sfName = "Rumplestiltskin";
 }
+*/
+// 2.)
+/*
+int main()
+{
+	string sMName = "Lee";
+
+	if (sMName.length() < 5)
+	{
+		for (int i = 0; i < sMName.length(); i++)
+		{
+			cout << sMName[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << sMName[i];
+		}
+	}
+	
+}
+*/
+// 3.)
+/*
+int main()
+{
+	string sLName = "Soroka";
+
+	for (int i = sLName.length(); i > -1; i--)
+	{
+		cout << sLName[i];
+	}
+}
+*/
+
+// CT FUNCTIONS
+// 1.)
+/*
+void smF (int a, int b, int c);
+*/
+// 2.)
+/*
+int smF(int a, int b)
+{
+	if (a < b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+*/
+// 3.)
+/*
+string Capitalize(string original)
+{
+	string output = original;
+	toupper(output[0]);
+
+	for (int i = 0; i < output.length(); i++)
+	{
+		if (output[i] == ' ')
+		{
+			toupper(output[i + 1]);
+		}
+	}
+	return output;
+}
+*/
