ae796a1810d44eb6ee33e4a4155a36f591b2e625
Joseph Soroka <sorokaj@rsd407.org>
2022-02-03 08:58:19 -0800

    2/3/22

---
+++ GS10-05a/GS10-05a.cpp
@@ -0,0 +1,36 @@
+/*
+Author: Joey Soroka
+File Name:
+Project Name:
+Purpose:
+Pseudocode:
+Maintenance Log:
+
+*/
+
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <iostream>
+#include <string> // Needed for string
+#include <stdlib.h> // Needed for srand
+#include <time.h> // Needed for srand
+#include <conio.h> 
+#include <vector>
+#include <windows.h> // Uses functions named after shapes beware for classes
+#include <chrono> // needed for sleep for
+#include <thread>
+#include <math.h>
+#include <sstream> // needed for files
+#include <fstream> // needed for files
+#include <cwchar>
+#include <algorithm> // needed for vector sort
+
+using namespace std;
+using namespace std::this_thread; // needed for sleep for
+using namespace std::chrono; // needed for sleep for
+
+int main()
+{
+
+}
+
