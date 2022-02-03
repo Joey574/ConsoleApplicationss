a4b00296d5de0ec6f455e2c62e76c85f65309f7f
Joseph Soroka <sorokaj@rsd407.org>
2022-02-03 09:30:01 -0800

    2/3/22

--- GS10-05a/GS10-05a.cpp
+++ GS10-05a/GS10-05a.cpp
@@ -31,6 +31,39 @@ using namespace std::chrono; // needed for sleep for
 
 int main()
 {
+	vector<vector<int>> a
+	{
+		{9, 5, 7},
+		{1, 2, 3},
+		{1, 2, 3}	
+	};
 
+	vector<vector<int>> b
+	{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3}
+	};
+
+	vector<vector<int>> c;
+
+	for (int i = 0; i < 3; i++)
+	{
+		vector<int> ans;
+		for (int p = 0; p < 3; p++)
+		{
+			for (int c = 0; c < 3; c++)
+			{
+				ans.push_back(a[p][c] * b[c][i]);
+				cout << ans[c] << endl;
+			}
+			ans[0] = ans[0] + ans[1] + ans[2];
+			cout << ans[0];
+			_getch();
+		}
+	}
+
+
+	return 0;
 }
 
